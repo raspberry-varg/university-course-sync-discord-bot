7a4fd67eec85e0953fa26659156d5bb978970d1a
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2021-11-08 21:56:13 -0700

    Simple ping status command

---
+++ commands/test/ping.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+module.exports = {
+    name: 'ping',
+    description: 'Replies with Pong, and your status!',
+    guilds: ['903924793542311947'],
+    async execute( interaction ) {
+        await interaction.reply({ content: 'Pong! Loading your stats...', ephemeral: true });
+
+        const User = mongoose.model('User', require('../../database/schemas/user').userSchema );
+        const found = await User.findOne({ userId: interaction.user.id }).exec();
+        if ( found )
+            return await interaction.editReply({ content: "Pong! Stats loaded!\nYou're already registered! You are userId: " + found.userId + " and joined the bot on " + new Date( found.joinedBot ) });
+        else {
+            await interaction.editReply({ content: "Pong! Stats loaded!\nNo user profile found in the system! If you'd like to get started, go ahead and use `/register` to get started!" });
+        }
+    },
+};
