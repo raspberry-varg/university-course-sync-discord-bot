0832d6e1af8ff28cdf9e00da272601fe4655d6b2
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2021-11-08 21:43:34 -0700

    Finalize user & server check. Final version.

--- commands/account/classes.js
+++ commands/account/classes.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+const User = mongoose.model('User', require('../../database/schemas/user').userSchema );
+const Server = mongoose.model('Server', require('../../database/schemas/server') );
 const { ApplicationCommandOptionType: OptionTypes } = require('discord-api-types/v9');
 const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
-const AddMenu = require('../../classes/AddMenu');
+const ClassesMenu = require('../../classes/ClassesMenu');
 
 module.exports = {
     name: 'classes',
@@ -14,28 +16,37 @@ module.exports = {
         var embed = new MessageEmbed({ color: interaction.client.config.colors.positive });
 
         // search for user in the database
-        const User = mongoose.model('User', require('../../database/schemas/user').userSchema );
-        const found = await User.findOne({ userId: interaction.user.id }).exec();
+        const foundUser = await User.findOne({ userId: interaction.user.id }).exec();
+        const foundServer = await Server.findOne({ guildId: interaction.guild.id }).exec();
 
-        // if a user is found, notify of registration, else prompt with registration
-        if ( !found ) {
+        // if no user is found, prompt with registration
+        if ( !foundUser ) {
             embed
                 .setTitle("♦️ Woah! You're not registered yet!")
                 .setDescription("You aren't in the system!\nTo gain full access to the features of this bot, get started by using `/register`!");
             return await interaction.editReply({ embeds: [ embed ] });
         }
-        else {
 
-            found.classes?.set('cs278');
-            found.classes = new Map();
-            found.classes.set('cs', ['272', '278']);
-            found.classes.set('engl', ['1102', '2210']);
-            await found.save();
-            console.log( found.classes );
-            console.log("Starting AddMenu...");
-            return new AddMenu( interaction, found );
-
-
+        // if no guild is found, prompt with notice
+        if ( !foundServer ) {
+            embed
+                .setTitle('⚠️ Woah there! Your server admin or admins have not set this bot up yet!')
+                .setDescription("If you are said admin, get started right away along with getting the full terms and conditions by typing `/serverdash` !");
         }
+
+
+        if ( !foundUser.classes ) foundUser.classes = new Map();
+
+        foundServer.any = false;
+        foundServer.courseSpecific.delete( 'cs' );
+        foundServer.courseType.set('cs', 'cs');
+        await foundServer.save();
+        console.log( foundUser.classes );
+        console.log("Starting AddMenu...");
+        
+        return new ClassesMenu( interaction, foundUser, foundServer ).startPage();
+
+
+
     },
 };
