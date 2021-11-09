a3604df0759a7030b68c4c304568526f44adb7ae
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2021-11-08 21:58:19 -0700

    Verify only one course against server config

---
+++ classes/VerifyOneClassSupport.js
@@ -0,0 +1,27 @@
+const { Mongoose } = require("mongoose");
+
+/**
+ * Verify that the server allows this course.
+ * @param {Array<String>} courseSplit
+ * @param {Mongoose.module} serverData 
+ */
+function verifyClassSupport( courseSplit, serverData ) {
+
+    // check blacklist
+    if ( serverData.courseBlacklist.has( courseSplit[0] ) && serverData.courseBlacklist.get( courseSplit[0] ).has( courseSplit[1] ) )
+        return false;
+    
+    // check course specific type
+    if ( serverData.courseSpecific.has( courseSplit[0] ) && serverData.courseSpecific.get( courseSplit[0] ).contains( courseSplit[1] ) )
+        return true;
+    
+    // else check course global type as specific overrides
+    else if ( !serverData.courseSpecific.has( courseSplit[0] ) && serverData.courseType.has( courseSplit[0] ) )
+        return true;
+    
+    // if all checks fail, return false
+    else
+        return false;
+}
+
+module.exports = verifyClassSupport;
