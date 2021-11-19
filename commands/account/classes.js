138ab2cdff1c6a2864341ee14beb803ef84e0c53
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2021-11-19 01:46:59 -0700

    Fixed reference error, reformatted mongoose calls Removed test cases Cached server in user data if not already

--- commands/account/classes.js
+++ commands/account/classes.js
@@ -1,14 +1,14 @@
+const { MessageEmbed } = require('discord.js');
 const mongoose = require('mongoose');
-const User = mongoose.model('User', require('../../database/schemas/user').userSchema );
-const Server = mongoose.model('Server', require('../../database/schemas/server') );
-const { ApplicationCommandOptionType: OptionTypes } = require('discord-api-types/v9');
-const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
-const ClassesMenu = require('../../classes/ClassesMenu');
+const serverSchema = require('../../database/schemas/server');
+const { userSchema } = require('../../database/schemas/user');
+
+const ClassesMenu = require('../../classes/menus/ClassesMenu');
 
 module.exports = {
     name: 'classes',
     description: `Add or remove classes all in one menu!`,
-    guilds: ['903924793542311947'],
+    guilds: ['903924793542311947', '910745675006877757'],
     global: false,
     async execute( interaction ) {
         
@@ -16,6 +16,8 @@ module.exports = {
         var embed = new MessageEmbed({ color: interaction.client.config.colors.positive });
 
         // search for user in the database
+        const User = mongoose.model('User', userSchema );
+        const Server = mongoose.model('Server', serverSchema );
         const foundUser = await User.findOne({ userId: interaction.user.id }).exec();
         const foundServer = await Server.findOne({ guildId: interaction.guild.id }).exec();
 
@@ -34,14 +36,9 @@ module.exports = {
                 .setDescription("If you are said admin, get started right away along with getting the full terms and conditions by typing `/serverdash` !");
         }
 
+        // cache server
+        if ( !foundUser.cachedServers.includes( interaction.guild.id ) ) foundUser.cachedServers.push( interaction.guild.id );
 
-        if ( !foundUser.classes ) foundUser.classes = new Map();
-
-        // foundServer.any = false;
-        // foundServer.courseSpecific.delete( 'cs' );
-        // foundServer.courseType.set('cs', 'cs');
-        // await foundServer.save();
-        // console.log( foundUser.classes );
         console.log("Starting ClassesMenu...");
         
         return new ClassesMenu( interaction, foundUser, foundServer ).startPage();
