cec5993a67b4019ae78262ea725d1769c88e6a6c
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-01-08 04:28:41 -0700

    Refactored for less redundancy & new JSON support

---
+++ app/classes/FormatSubject.js
@@ -0,0 +1,25 @@
+const { Client } = require("discord.js");
+
+/**
+ * Validate input to ensure course is valid.
+ * @param {string} input The input to validate.
+ * @param {Client} client The Discord Client.
+ */
+function validateSubjectInput( input, client ) {
+
+    // format to ABBREV
+    input = input.trim().replace(/[^a-zA-Z]/g, '').toUpperCase();
+
+    // validate
+    let courseSearch = client.courses.has( input ) ? input : false;
+    console.log({ input: input, courseSearch: courseSearch });
+
+    // check common name to short-hand
+    if ( !courseSearch )
+        return client.commonNames.has( input.toLowerCase() ) ? client.commonNames.get( input.toLowerCase() ).toLowerCase() : false;
+    else
+        return courseSearch?.toString().toLowerCase();
+
+}
+
+module.exports = validateSubjectInput;
