da4ece2d04aec8d712b40d873d08e246719c7392
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-01-09 03:18:11 -0700

    Commented out excessive debug logs This will fully be revamped when converted to TS

--- app/classes/RefreshProxyUserClasses.js
+++ app/classes/RefreshProxyUserClasses.js
@@ -14,14 +14,14 @@ async function refreshProxyUserClasses( guildSnowflake, foundUser, foundServer )
     
     const debugId = `[${ ( await guildSnowflake.members.fetch( foundUser.userId ) ).user.username }(${foundUser.userId})] `;
     // cycle through each course and check against server data
-    console.log( debugId + "CYCLING THROUGH EACH COURSE AND CHECKING AGAINST SERVER DATA");
-    console.log( debugId +  foundUser.classes );
+    // console.log( debugId + "CYCLING THROUGH EACH COURSE AND CHECKING AGAINST SERVER DATA");
+    // console.log( debugId +  foundUser.classes );
     for ( [ key, array ] of foundUser.classes ) {
     // await foundUser.classes.forEach( async ( array, key ) => {
-        console.log({ array: array, key: key });
+        // console.log({ array: array, key: key });
 
-        console.log( debugId + "\n\nCURRENT KEY: ");
-        console.log( key );
+        // console.log( debugId + "\n\nCURRENT KEY: ");
+        // console.log( key );
         /*
             * Okay so:
             * 
@@ -34,11 +34,11 @@ async function refreshProxyUserClasses( guildSnowflake, foundUser, foundServer )
 
         // check to ensure the key is valid in the server
         if ( !verifyOneCourseTypeSupport( key, foundServer ) ) {
-            console.log( debugId + "THIS ENTIRE SUBJECT IS NOT SUPPORTED");
+            // console.log( debugId + "THIS ENTIRE SUBJECT IS NOT SUPPORTED");
             continue;
         }
-        else
-            console.log( debugId + "REFRESHING " + key);
+        // else
+        //     console.log( debugId + "REFRESHING " + key);
         
         //// THIS WORKS
         // ! nevermind
@@ -90,7 +90,7 @@ async function refreshProxyUserClasses( guildSnowflake, foundUser, foundServer )
         // cycle through each course within the subject type ( I.E.   { CS: [...] }, { ENGL: [...] } )
         // await array.forEach( async value => {
         for ( value of array ) {
-            console.log( debugId + "\n\tCURRENT CLASS: " + value );
+            // console.log( debugId + "\n\tCURRENT CLASS: " + value );
             
             // if server supports the course
             if ( verifyClassSupport([ key, value ], foundServer ) ) {
@@ -105,39 +105,39 @@ async function refreshProxyUserClasses( guildSnowflake, foundUser, foundServer )
                     }
                     foundServer.courseData.get( key ).set( value, courseConfig );
                     // foundServer.courseData = new Map( foundServer.courseData );
-                    console.log( debugId +  foundServer.courseData.get( key ) );
+                    // console.log( debugId +  foundServer.courseData.get( key ) );
                 }
                 else
-                    console.log( debugId + "\tCOURSE CONFIG FOUND.");
+                    // console.log( debugId + "\tCOURSE CONFIG FOUND.");
 
-                console.log( debugId + "\tPARENT: " + courseCategory.id);
+                // console.log( debugId + "\tPARENT: " + courseCategory.id);
 
                 // create new role and channel dedicated to the course
-                console.log( debugId + "\tcourseConfig:")
-                console.log(courseConfig);
+                // console.log( debugId + "\tcourseConfig:")
+                // console.log(courseConfig);
 
-                console.log( debugId + "\tFetching role...");
+                // console.log( debugId + "\tFetching role...");
                 var newRoleCreated = false;
                 const fetchedRole = await guildSnowflake.roles.fetch( courseConfig.roleId.toString() )
                     .then( found => {
-                        console.log( debugId + `Role successfully found! Role: ${found.name}(${found.id})`);
+                        // console.log( debugId + `Role successfully found! Role: ${found.name}(${found.id})`);
                         guildSnowflake.members.fetch( foundUser.userId ).then( m => m.roles.add( found )).catch( e => console.error( e ) );
                         return found;
                     })
                     .catch( async () => {
-                        console.log( debugId + "\tNo role found, creating...");
+                        // console.log( debugId + "\tNo role found, creating...");
                         return await guildSnowflake.roles.create({
                             name: key.toUpperCase() + value,
                             permissions: [],
                             color: foundServer.roleColor,
                         })
                         .then( created => {
-                            console.log( debugId + "New role created.");
+                            // console.log( debugId + "New role created.");
                             newRoleCreated = true;
                             
                             // save to database
                             foundServer.courseData.get( key ).get(value).roleId = created.id;
-                            console.log(foundServer.courseData.get( key ).get(value));
+                            // console.log(foundServer.courseData.get( key ).get(value));
 
                             guildSnowflake.members.fetch( foundUser.userId ).then( m => m.roles.add( created )).catch( e => console.error( e ) );
 
@@ -146,14 +146,14 @@ async function refreshProxyUserClasses( guildSnowflake, foundUser, foundServer )
                     });
 
                 // fetch given channel
-                console.log( debugId + "Fetching channel...");
+                // console.log( debugId + "Fetching channel...");
                 const fetchedChannel = await guildSnowflake.channels.fetch( courseConfig.channelId.toString() )
                     .then( found => {
-                        console.log( debugId + `Channel successfully found! ${found.name}(${found.id})`);
+                        // console.log( debugId + `Channel successfully found! ${found.name}(${found.id})`);
                         return found;
                     })
                     .catch( async () => {
-                        console.log( debugId + "\tNo channel found, creating...");
+                        // console.log( debugId + "\tNo channel found, creating...");
                         return await guildSnowflake.channels.create( key.toUpperCase() + value, {
                             type: 0, // guild text
                             parent: courseCategory?.id || courseCategory,
@@ -170,10 +170,10 @@ async function refreshProxyUserClasses( guildSnowflake, foundUser, foundServer )
                                 },
                             ],
                         }).then( created => {
-                            console.log( debugId + `New channel created. ${created.name}(${created.id})`);
+                            // console.log( debugId + `New channel created. ${created.name}(${created.id})`);
                             // save to database
                             foundServer.courseData.get( key ).get(value).channelId = created.id;
-                            console.log( foundServer.courseData.get(key).get(value) );
+                            // console.log( foundServer.courseData.get(key).get(value) );
                             // foundServer.courseData.get( key ).set( value, {...data} );
 
                             return created;
@@ -199,13 +199,13 @@ async function refreshProxyUserClasses( guildSnowflake, foundUser, foundServer )
                     });
                 }
             }
-            else
-                console.log( debugId + '\tThis class is not supported.');
+            // else
+                // console.log( debugId + '\tThis class is not supported.');
 
         }; // end for each
         console.log();
     }//); // end outer loop
-    console.log( debugId + "THE OUTER LOOP HAS BEEN BROKEN");
+    // console.log( debugId + "THE OUTER LOOP HAS BEEN BROKEN");
 
     // save changes
     foundServer.markModified('courseData');
