e132bad078c8f292f51ac44104c88007fdc85ab4
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-01-27 04:59:46 +0000

    Removed duplicate

--- src/util/Refresh/RefreshUserClasses.js
+++ src/util/Refresh/RefreshUserClasses.js
@@ -6,7 +6,7 @@ const fetchCourseCategory = require('./fetchCourseCategory');
 const verifyClassSupport = require('../VerifySupport/VerifyOneClassSupport');
 const verifyOneSubjectSupport = require('../VerifySupport/VerifyOneSubjectSupport');
 const refreshProxyUserClasses = require('./RefreshProxyUserClasses');
-const { Interaction } = require('coursesync');
+// const { Interaction } = require('coursesync');
 
 
 /**
